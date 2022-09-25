820caf28e366c07977d75bf1d4bc36e219dbd7d6
chmonster <perry.moore@gmail.com>
2022-09-25 18:08:32 -0230

    initial server setup in progress

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import { Client } from 'boardgame.io/react'
+// import { Local } from 'boardgame.io/multiplayer'
+import { SocketIO } from 'boardgame.io/multiplayer'
 import { Pente } from './Game'
 import { PenteBoard } from './Board'
 
-const App = Client({
+const PenteClient = Client({
   game: Pente,
   board: PenteBoard,
+  // multiplayer: Local({
+  //   // Enable localStorage cache.
+  //   persist: true,
+
+  //   // Set custom prefix to store data under. Default: 'bgio'.
+  //   storageKey: 'bgio',
+  // }),
+  multiplayer: SocketIO({ server: 'localhost:8000' }),
 })
+
+const App = () => (
+  <div>
+    <PenteClient />
+  </div>
+)
 
 export default App
